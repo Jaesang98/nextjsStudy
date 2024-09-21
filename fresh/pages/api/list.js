be1808af433dce11b47da27e15f97b1438fc30ab
jsnam <skawotkd12@naver.com>
2024-09-21 20:05:18 +0900

    jsnma

--- fresh/pages/api/list.js
+++
@@ -1,9 +0,0 @@
-import { connectDB } from "@/util/database";
-
-export default async function List(method, res) {
-    const client = await connectDB;
-    const db = client.db("forum");
-    let result = await db.collection('post').find().toArray();
-
-    res.status(200).json(result);
-}
--- fresh/pages/api/test.js
+++
@@ -1,4 +0,0 @@
-export default function handler(요청, 응답) {
-    console.log(123);
-    return 응답.status(200).json('안녕')
-}
